e7a683a31cda5baecd0977e4f47ea482b4c5b498
TrendBattles <jackhackerss123@gmail.com>
2025-10-23 22:54:36 -0700

    Initializing the project

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <filesystem>
 
-int main() {
+const std::string project_dir = PROJECT_DIR;
+
+int main(int argv, char* args[]) {
 
 	sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({ 600, 600 }), "Go");
 
@@ -11,9 +13,11 @@ int main() {
 		return EXIT_FAILURE;
 	}
 
-	const std::string board_path = project_dir + "\\assets\\board\\board.png";
+	const std::filesystem::path board_path = std::filesystem::absolute(project_dir + "assets/board/board.png");
 
-	sf::Texture texture(board_path);
+	sf::Texture texture;
+	bool success = texture.loadFromFile(board_path.string());
+
 	sf::Sprite sprite(texture, sf::IntRect({ 0, 0 }, { 600, 600 }));
 
 	while (window.isOpen()) {
@@ -22,16 +26,17 @@ int main() {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is <sf::Event::Closed>()) {
 				std::cerr << "Closing the window" << std::endl;
-				window.close(); 
-				
+				window.close();
+
 				break;
 			}
-		} 
+		}
 
 
 		window.draw(sprite);
 
 		window.display();
 	}
+
 	return EXIT_SUCCESS;
 }
