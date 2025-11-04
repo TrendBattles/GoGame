e8646ab788bb2f7224120fdbf16155e106608e09
TrendBattles <jackhackerss123@gmail.com>
2025-11-04 02:15:29 -0800

    -Added rules -Need reviews and more suggestions in optimizing code / performance

--- include/Board.hpp
+++ include/Board.hpp
@@ -9,7 +9,7 @@
 class Board {
 public:
 	Board();
-	
+
 	void loadBoard(const std::string _str_source = "");
 	void loadPieces();
 
@@ -18,9 +18,9 @@ public:
 
 	void drawBoard();
 	void drawState();
-	void placeAt(sf::Vector2f mouse_pos = sf::Vector2f{ 0, 0 });
-	void drawShadow(sf::Vector2f mouse_pos = sf::Vector2f { 0, 0 });
-	
+	void tryPlacingAt(sf::Vector2f mouse_pos = sf::Vector2f{ 0, 0 });
+	void drawShadow(sf::Vector2f mouse_pos = sf::Vector2f{ 0, 0 });
+
 	void setState(int x, int y, int c);
 	int getState(int x, int y);
 	bool emptyCell(int x, int y);
@@ -30,26 +30,24 @@ public:
 	bool getTurn();
 	
 	std::vector <std::pair <int, int>> findComponent(int x, int y, int cell_id);
+	std::vector <std::pair <int, int>> capturedPositions(int x, int y, bool turn);
 
-	bool KORule(int x, int y, bool turn);
-	bool existCapture(int x, int y, bool turn);
 	bool possibleToPlace(int x, int y, bool turn);
-
-
 private:
 	Texture surface;
 	Texture pieces[2];
 
-	std::vector <std::string> state;
+	std::vector <std::string> state_list;
+	int state_pointer;
 
 	int row = -1, column = -1;
 	bool current_turn;
 
-	std::pair <int, int> single_cell_capture;
-
 	sf::Vector2f PIECE_SCALE;
 	sf::Vector2f offset;
 	sf::Vector2f gapX, gapY;
+
+	void placePieceAt(int x, int y);
 };
 
 #endif
--- src/Board.cpp
+++ src/Board.cpp
@@ -11,7 +11,7 @@ Board::Board() {
 	gapY = sf::Vector2f(0, 65);
 
 	current_turn = 0;
-	single_cell_capture = std::make_pair(-1, -1);
+	state_pointer = 0;
 }
 
 void Board::loadBoard(const std::string _str_source) {
@@ -31,7 +31,8 @@ void Board::setSize(int _i_row, int _i_column) {
 	row = _i_row;
 	column = _i_column;
 
-	state = std::vector <std::string>(row, std::string(column, '.'));
+	state_pointer = 0;
+	state_list.assign(1, std::string(row * column, '.'));
 }
 
 std::pair <int, int> Board::getSize() {
@@ -55,7 +56,7 @@ void Board::drawState() {
 	}
 }
 
-void Board::placeAt(sf::Vector2f mouse_pos) {
+void Board::tryPlacingAt(sf::Vector2f mouse_pos) {
 	const float RADIUS = 25;
 
 	for (int r = 0; r < row; ++r) {
@@ -66,9 +67,9 @@ void Board::placeAt(sf::Vector2f mouse_pos) {
 			if (!possibleToPlace(r, c, current_turn)) continue;
 
 			std::cout << r << " " << c << "\n";
-			setState(r, c, current_turn);
+			placePieceAt(r, c);
 
-			current_turn = 1 ^ current_turn;
+			pieces[current_turn].draw(cur_pos);
 		}
 	}
 }
@@ -84,17 +85,34 @@ void Board::drawShadow(sf::Vector2f mouse_pos) {
 			if (!possibleToPlace(r, c, current_turn)) continue;
 
 			pieces[current_turn].draw(cur_pos, 0.6);
+			return;
 		}
 	}
 }
 
+void Board::placePieceAt(int x, int y) {
+	setState(x, y, current_turn);
+	current_turn = 1 ^ current_turn;
+}
+
 void Board::setState(int x, int y, int c) {
+	/*
+		This updates the board state (including rules)
+	*/
 	if (x < 0 || x >= row || y < 0 || y >= column) {
 		std::cerr << "Setting a null cell (" << x << "," << y << ")\n";
 		return;
 	}
 	
-	state[x][y] = c == -1 ? '.' : c + '0';
+	state_list.erase(state_list.begin() + state_pointer + 1, state_list.end());
+	std::vector <std::pair <int, int>> captured = capturedPositions(x, y, c);
+
+	state_list.push_back(state_list.back());
+	state_list[++state_pointer][x * column + y] = c == -1 ? '.' : c + '0';
+
+	for (std::pair <int, int> point : captured) {
+		state_list[state_pointer][point.first * column + point.second] = '.';
+	}
 }
 
 
@@ -102,13 +120,13 @@ void Board::setState(int x, int y, int c) {
 int Board::getState(int x, int y) {
 	if (outsideBoard(x, y)) return -1;
 
-	return state[x][y] == '.' ? -1 : state[x][y] - '0';
+	return state_list[state_pointer][x * column + y] == '.' ? -1 : state_list[state_pointer][x * column + y] - '0';
 }
 
 bool Board::emptyCell(int x, int y) {
 	if (outsideBoard(x, y)) return false;
 
-	return state[x][y] == '.';
+	return state_list[state_pointer][x * column + y] == '.';
 }
 
 bool Board::outsideBoard(int x, int y) {
@@ -122,6 +140,7 @@ void Board::setTurn(bool who) {
 bool Board::getTurn() {
 	return current_turn;
 }
+
 
 std::vector <std::pair <int, int>> Board::findComponent(int x, int y, int cell_id) {
 	/*
@@ -157,72 +176,42 @@ std::vector <std::pair <int, int>> Board::findComponent(int x, int y, int cell_i
 	return _cell_queue;
 }
 
-bool Board::KORule(int x, int y, bool turn) {
+std::vector <std::pair <int, int>> Board::capturedPositions(int x, int y, bool turn) {
 	/*
-		To check if we place the piece for the target turn in (x, y), it violates KO rule.
-		Note that (x, y) has to be empty.
+		Find all captures if place (x, y) in the target turn.
 	*/
-	if (single_cell_capture == std::make_pair(-1, -1)) return false;
 
-	if (!emptyCell(x, y)) {
-		std::cerr << "Error: (" << x << "," << y << ") is not empty to check KO rule\n";
-		return false;
-	}
-
-	const int delta_x[4] = { +1, -1, 0, 0 };
-	const int delta_y[4] = { 0, 0, +1, -1 };
+	std::vector <std::pair <int, int>> subset;
+	
+	const int delta_x[4] = { -1, +1, 0, 0 };
+	const int delta_y[4] = { 0, 0, -1, +1 };
+	std::vector <std::vector <int>> marked(row, std::vector <int>(column));
 
 	for (int iter = 0; iter < 4; ++iter) {
-		int adj_x = single_cell_capture.first + delta_x[iter];
-		int adj_y = single_cell_capture.second + delta_y[iter];
+		int new_x = x + delta_x[iter], new_y = y + delta_y[iter];
+		if (getState(new_x, new_y) != (1 ^ turn)) continue;
 
+		std::vector <std::pair <int, int>> candidates = findComponent(new_x, new_y, 1 ^ turn);
+		int any_liberty = false;
 
-		if (emptyCell(adj_x, adj_y) && x != adj_x && y != adj_y) return true;
-	}
+		for (std::pair <int, int> point : candidates) {
+			marked[point.first][point.second] = true;
 
-	return false;
-}
+			for (int dir = 0; dir < 4 && !any_liberty; ++dir) {
+				if (!emptyCell(point.first + delta_x[dir], point.second + delta_y[dir])) continue;
 
-bool Board::existCapture(int x, int y, bool turn) {
-	/*
-		If we place (x, y) in the target turn, will there be captures ?
-	*/
-	if (!emptyCell(x, y)) {
-		std::cerr << "Error: (" << x << "," << y << ") is not empty to check existing captures \n";
-		return false;
-	}
-	
-	const int delta_x[4] = { +1, -1, 0, 0 };
-	const int delta_y[4] = { 0, 0, +1, -1 };
-
-	for (int i = 0; i < row; ++i) {
-		for (int j = 0; j < column; ++j) {
-			if (getState(i, j) != (1 ^ turn)) continue;
-				
-			/*
-				Liberty -> No capture
-			*/
-
-			int liberty = false;
-
-			for (int iter = 0; iter < 4; ++iter) {
-				int new_i = i + delta_x[iter];
-				int new_j = j + delta_y[iter];
-
-				if (outsideBoard(new_i, new_j)) continue;
-
-				if (!emptyCell(new_i, new_j)) continue;
-				if (new_i == x && new_j == y) continue;
-
-				liberty = true;
-				break;
+				any_liberty |= x != point.first + delta_x[dir] || y != point.second + delta_y[dir];
 			}
+		}
 
-			if (!liberty) return true;
+		if (any_liberty) continue;
+		
+		for (std::pair <int, int>& point : candidates) {
+			subset.emplace_back(point);
 		}
 	}
 
-	return false;
+	return subset;
 }
 
 bool Board::possibleToPlace(int x, int y, bool turn) {
@@ -238,24 +227,30 @@ bool Board::possibleToPlace(int x, int y, bool turn) {
 			Check if this move is a capture that doesn't violate KO rule or not.
 	*/
 
+	std::vector <std::pair <int, int>> captured = capturedPositions(x, y, turn);
 
-	std::vector <std::pair <int, int>> _component = std::move(findComponent(x, y, turn));
-	const int delta_x[4] = { +1, -1, 0, 0 };
-	const int delta_y[4] = { 0, 0, +1, -1 };
+	if (captured.empty()) {
+		const int delta_x[4] = { -1, +1, 0, 0 };
+		const int delta_y[4] = { 0, 0, -1, +1 };
 
-	int any_liberty = false;
-	for (std::pair <int, int> point : _component) {
-		for (int iter = 0; iter < 4; ++iter) {
-			any_liberty |= (int) emptyCell(point.first + delta_x[iter], point.second + delta_y[iter]);
+		std::vector <std::pair <int, int>> component = findComponent(x, y, turn);
+		int any_liberty = false;
+
+		for (std::pair <int, int> point : component) {
+			for (int iter = 0; iter < 4; ++iter) {
+				any_liberty |= (int)emptyCell(point.first + delta_x[iter], point.second + delta_y[iter]);
+			}
 		}
-
-		if (any_liberty) break;
+		
+		return any_liberty;
 	}
 
-	if (!any_liberty) {
-		return !KORule(x, y, turn) && existCapture(x, y, turn);
+	std::string current_state = state_list[state_pointer];
+	current_state[x * column + y] = turn + '0';
+
+	for (std::pair <int, int> point : captured) {
+		current_state[point.first * column + point.second] = '.';
 	}
 
-
-	return !existCapture(x, y, turn) || !KORule(x, y, turn);
+	return state_pointer == 0 || current_state != state_list[state_pointer - 1];
 }
